ca9634dbdf8227c8d4097b7664055ade1ba59021
Daniyutak <dtakara1101@gmail.com>
2026-07-28 22:55:45 -0300

    feat: adiciona uma função que padroniza a pesquisa

--- backend/src/services/SuggestionService.js
+++ backend/src/services/SuggestionService.js
@@ -1,18 +1,25 @@
 import suggestions from "../data/suggestions.json" with { type: "json" };
+import normalizeText from "../utils/normalizeText.js";
 
 class SuggestionService {
 
     getSuggestions(query) {
 
-        if (!query) {
-            return [];
-        }
+         if (!query) {
+        return [];
+    }
 
-        return suggestions.filter((suggestion) =>
-            suggestion.text
-                .toLowerCase()
-                .includes(query.toLowerCase())
-        );
+    const normalizedQuery = normalizeText(query);
+
+    return suggestions.filter((suggestion) => {
+
+        const normalizedSuggestion =
+            normalizeText(suggestion.text);
+
+        return normalizedSuggestion.includes(normalizedQuery);
+
+    });
+
 
     }
 
